34df250e9dab571fc81b330eb2826587bb3fd66d
Steve JobsEleanor <eleanorabrow@gmail.com>
2023-03-26 13:46:32 -0400

    updated calculator

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandandTextElement = currentOperandandTextElement
+  }
+
+  clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined 
+  }
+
+  delete() {
+
+  }
+  appendNumber(number) {
+    this.currentOperand = number
+  }
+  chooseOperation(operation) {
+
+  }
+  compute() {
+
+  }
+  updateDisplay() {
+    this.currentOperandandTextElement.innerText = this.currentOperand
+  }
+}
+
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operations]')
 const equalsButton = document.querySelector('[data-equals]')
@@ -5,3 +35,14 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButtonButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandandTextElement = document.querySelector('[data-current-operand]')
+
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandandTextElement)
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+
+})
